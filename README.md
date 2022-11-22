345ee9aff94e5cba5ee416ef3e0b911348a613bd
Yash Patki <yashom.patki@gmail.com>
2022-11-21 23:03:10 -0800

    Added explore and expose

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Lab 8 - Starter
+
+Yash Patki
+
+1. Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+
+**Ans)** Within a Github Action workflow that runs whenever new code is pushed to automatically run the tests and report the results efficiently. 
+
+2. Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+
+**Ans)** No, end to end tests are used to emulate user action from start to finish and check if the application is wroking correctly, but they are not used to test each individual function.
+
+3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+**Ans)** No, unit tests are used to test individual functions and not the entire application and since the messaging feature is the main part of the application containing several features, it would be better to avoid unit testing it.
+
+4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+**Ans)** Yes, unit tests are used to test individual parts of code and since the max message length feature is a small part of the messaging feature, it would be better to use unit testing.
+
+
+
+# Screenshots
+
+![Screenshot 1](screenshots/Screenshot%20(401).png)
+![Screenshot 2](screenshots/Screenshot%20(402).png)
+![Screenshot 3](screenshots/Screenshot%20(403).png)
+![Screenshot 4](screenshots/Screenshot%20(404).png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,88 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// isPhoneNumber Tests
+test('isPhoneNumber() returns true for valid phone numbers', () => {
+    expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+
+test('isPhoneNumber() returns true for valid phone numbers', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('isPhoneNumber() returns false for invalid phone numbers', () => {
+    expect(functions.isPhoneNumber('1234DFW7890')).toBe(false);
+});
+
+test('isPhoneNumber() returns false for invalid phone numbers', () => {
+    expect(functions.isPhoneNumber('123 AAA 7890')).toBe(false);
+});
+
+// isEmail Tests
+test('isEmail() returns true for valid emails', () => {
+    expect(functions.isEmail('fury2020@gmail.com')).toBe(true);
+});
+
+test('isEmail() returns true for valid emails', () => {
+    expect(functions.isEmail('hellopanda@yahoo.com')).toBe(true);
+});
+
+test('isEmail() returns false for invalid emails', () => {
+    expect(functions.isEmail('hello world')).toBe(false);
+});
+
+test('isEmail() returns false for invalid emails', () => {
+    expect(functions.isEmail('123@gmail. com')).toBe(false);
+});
+
+// isStrongPassword Tests
+test('isStrongPassword() returns true for strong passwords', () => {
+    expect(functions.isStrongPassword('My21*is_*34*_*_Po')).toBe(false);
+});
+
+test('isStrongPassword() returns true for strong passwords', () => {
+    expect(functions.isStrongPassword('H23*el*lo*123*_1')).toBe(false);
+});
+
+test('isStrongPassword() returns false for weak passwords', () => {
+    expect(functions.isStrongPassword('hello')).toBe(true);
+});
+
+test('isStrongPassword() returns false for weak passwords', () => {
+    expect(functions.isStrongPassword('helloworld')).toBe(true);
+});
+
+// isDate Tests
+test('isDate() returns true for valid dates', () => {
+    expect(functions.isDate('12/31/2023')).toBe(true);
+});
+
+test('isDate() returns true for valid dates', () => {
+    expect(functions.isDate('1/1/2023')).toBe(true);
+});
+
+test('isDate() returns false for invalid dates', () => {
+    expect(functions.isDate('12/30/20')).toBe(false);
+});
+
+test('isDate() returns false for invalid dates', () => {
+    expect(functions.isDate('1/1/202')).toBe(false);
+});
+
+// isHexColor Tests 
+test('isHexColor() returns true for valid hex colors', () => {
+    expect(functions.isHexColor('#FFFFAA')).toBe(true);
+});
+
+test('isHexColor() returns true for valid hex colors', () => {
+    expect(functions.isHexColor('#0000FF')).toBe(true);
+});
+
+test('isHexColor() returns false for invalid hex colors', () => {
+    expect(functions.isHexColor('F4FFF')).toBe(false);
+});
+
+test('isHexColor() returns false for invalid hex colors', () => {
+    expect(functions.isHexColor('#000F0')).toBe(false);
+});
